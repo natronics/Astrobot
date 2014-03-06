b311cb7716724f94af733ace525b3017ca621169
Nathan Bergey <nathan.bergey@gmail.com>
2014-03-05 22:15:34 -0800

    remove ping code, we thing that works

--- ephemeris.py
+++ ephemeris.py
@@ -6,11 +6,6 @@ from zenircbot_api import ZenIRCBot
 # default redis server, future behavior will get config from python api
 zen = ZenIRCBot('localhost', 6379, 0)
 
-
-@zen.simple_command('ping', desc="respond to a person")
-def test(message):
-    return 'pongo'
-
 @zen.simple_command('sun', desc="compute sun angle, rise, set, etc.")
 def sun(message):
 
